a8cb5107c245a70f246a6218487a11fdbdc332ed
SIEUN CHOI <csieung@github.com>
2024-07-09 16:31:56 -0700

    Add: Chapter 6

---
+++ Assets/Develop/Chapter 6/Scripts/ColorChangeOnInput.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.PolySpatial.InputDevices;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+using UnityEngine.InputSystem.LowLevel;
+
+public class ColorChangeOnInput : MonoBehaviour
+{
+    void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();   
+    }
+    void Update()
+    {
+        if (Touch.activeTouches.Count > 0) // 활성화된 터치가 있는지 확인
+        {
+            foreach (var touch in Touch.activeTouches)
+            {
+                Debug.Log($"Touch detected: {touch.phase} at position {touch.screenPosition}");
+                if (touch.phase == TouchPhase.Began) // 터치가 되었는지 확인 
+                {
+                    SpatialPointerState touchData = EnhancedSpatialPointerSupport.GetPointerState(touch);// 터치 공간 포인터 상태 가져옴 
+                    if (touchData.targetObject != null && touchData.Kind != SpatialPointerKind.Touch)
+                    {
+                        Debug.Log($"Hit object");
+                        ChangeObjectColor(touchData.targetObject); // 터치가 된 객체 색상 변경 
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    void ChangeObjectColor(GameObject obj) // 객체 색상 변경 메서드 
+    {
+        Renderer objRenderer = obj.GetComponent<Renderer>();
+        if (objRenderer != null) // null이 아닌 경우 색상 변경 
+        {
+            objRenderer.material.color = new Color(Random.value, Random.value, Random.value); // 객체 색상 무작위 변경 
+        }
+    }
+}
---
+++ Assets/Develop/Chapter 6/Scripts/MoveObjectOnInput.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.PolySpatial.InputDevices;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+using UnityEngine.InputSystem.LowLevel;
+
+public class MoveObjectOnInput : MonoBehaviour
+{
+    private GameObject selectedObject; // 객체 저장 변수 
+    private Vector3 lastPosition; // 터치 위치 저장 변수 
+
+    void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    void Update()
+    {
+        if (Touch.activeTouches.Count > 0)
+        {
+            foreach (var touch in Touch.activeTouches)
+            {
+                SpatialPointerState touchData = EnhancedSpatialPointerSupport.GetPointerState(touch);
+
+                if (touchData.targetObject != null && touchData.Kind != SpatialPointerKind.Touch)
+                {
+                    if (touch.phase == TouchPhase.Began) // 터치 시작 터치가 움직이면 초기 위치를 기록
+                    {
+                        selectedObject = touchData.targetObject;
+                        lastPosition = touchData.interactionPosition;
+                    }
+                    else if (touch.phase == TouchPhase.Moved && selectedObject != null)
+                    {
+                        // 터치 이동 거리를 계산하여 객체 이동 
+                        Vector3 deltaPosition = touchData.interactionPosition - lastPosition;
+                        selectedObject.transform.position += deltaPosition;
+                        // 마지막 위치 업데이트 
+                        lastPosition = touchData.interactionPosition;
+                    }
+                }
+            }
+        }
+
+        if (Touch.activeTouches.Count == 0) // 선택한 개체 재설정 
+        {
+            selectedObject = null;
+        }
+    }
+}
